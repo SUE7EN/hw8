a60c9cd1a7ce2713a97da6197b941f9b0f2bd53c
Shuwen Zheng <55172213+SUE7EN@users.noreply.github.com>
2019-10-29 19:54:32 -0700

    Create leds.ino

---
+++ leds.ino
@@ -0,0 +1,62 @@
+int led = 5;
+int led2 = 13;
+int led3 = 6;
+int led4 = 7;
+void setup() {
+  // put your setup code here, to run once:
+  pinMode (led, OUTPUT);
+  pinMode (led2, OUTPUT);
+  pinMode (led3, OUTPUT);
+  pinMode (led4, OUTPUT);
+}
+
+void loop() {
+  digitalWrite (led, HIGH);
+  delay (200);
+  digitalWrite (led, LOW);
+  delay (200);
+  digitalWrite (led2, HIGH);
+  delay (200);
+  digitalWrite (led2, LOW);
+  delay (200);
+  digitalWrite (led3, HIGH);
+  delay (200);
+  digitalWrite (led3, LOW);
+  delay (200);
+  digitalWrite (led4, HIGH);
+  delay (200);
+  digitalWrite (led4, LOW);
+  delay (200);
+  digitalWrite (led, HIGH);
+  delay (100);
+  digitalWrite (led, LOW);
+  delay (100);
+  digitalWrite (led, HIGH);
+  delay (100);
+  digitalWrite (led, LOW);
+  delay (100);
+  digitalWrite (led2, HIGH);
+  delay (100);
+  digitalWrite (led2, LOW);
+  delay (100);
+  digitalWrite (led2, HIGH);
+  delay (100);
+  digitalWrite (led2, LOW);
+  delay (100);
+  digitalWrite (led3, HIGH);
+  delay (100);
+  digitalWrite (led3, LOW);
+  delay (100);
+  digitalWrite (led3, HIGH);
+  delay (100);
+  digitalWrite (led3, LOW);
+  delay (100);
+  digitalWrite (led4, HIGH);
+  delay (100);
+  digitalWrite (led4, LOW);
+  delay (100);
+  digitalWrite (led4, HIGH);
+  delay (100);
+  digitalWrite (led4, LOW);
+  delay (100);
+}
